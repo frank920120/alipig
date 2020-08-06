7c6e32c89a243c6abb1edec28ecce46ef7b69c1d
Jun fang <junfang@Juns-MacBook-Air.local>
2020-08-06 13:56:51 -0700

    adding pages and tabBar

---
+++ pages/strategy/strategy.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		strategy
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
